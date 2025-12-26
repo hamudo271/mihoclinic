bb6dc3f48a287bc39210258eeb604a3537a80107
조현도 <johyeondo@johyeondoui-Macmini.local>
2025-12-26 10:33:07 +0900

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AppRoutes } from './router';
 
 function App() {
   return (
-    <BrowserRouter basename={__BASE_PATH__}>
+    <HashRouter>
       <AppRoutes />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
